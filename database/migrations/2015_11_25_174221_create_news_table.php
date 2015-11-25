a683463ecd68600621573238e29d2815cddc070b
Nicolas Obara <nicolas.obara@gmail.com>
2015-11-25 22:09:07 +0100

    Ajout des migrations pour les news

---
+++ database/migrations/2015_11_25_174221_create_news_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->string('title', 200);
+            $table->string('slug', 250);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news', function(Blueprint $table) {
+            $table->dropForeign('user_id');
+        });
+        Schema::drop('news');
+    }
+}
