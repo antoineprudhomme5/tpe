90c739e39e44aabd7c2ab89f94ff270cd3138dc8
Nicolas Obara <nicolas.obara@gmail.com>
2015-12-16 15:58:40 +0100

    minor

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Topic;
 use App\Actualite;
-use DateTime;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
