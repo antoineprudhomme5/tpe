63dbc5c412a786d319401d6b42e213d01d1a2b28
Nicolas Obara <nicolas.obara@gmail.com>
2016-01-09 17:42:27 +0100

    update users seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,13 +12,39 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert(array(
-            array('name' => 'Prudhomme', 'firstname' => 'antoine', 'category_id' => 2, 'email' => 'antoineprudhomme5@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Obara', 'firstname' => 'nicolas', 'category_id' => 2, 'email' => 'user@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Doe', 'firstname' => 'John', 'category_id' => 1, 'email' => 'prof@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Lafont', 'firstname' => 'user1', 'category_id' => 2, 'email' => 'user1@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Labat', 'firstname' => 'user2', 'category_id' => 2, 'email' => 'user2@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Durand', 'firstname' => 'user3', 'category_id' => 2, 'email' => 'user3@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Dupont', 'firstname' => 'user4', 'category_id' => 2, 'email' => 'user4@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S')
+            array('name' => 'Prudhomme', 'firstname' => 'Antoine', 'category_id' => 2, 'email' => 'antoineprudhomme5@gmail.com',
+                'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S',
+                'avatar' => 'Qoo8OCzsNSO8ZPi5VyN6MAoirpAygWpYvb3twUAD.jpg',
+                'avatar_sm' => 'Qoo8OCzsNSO8ZPi5VyN6MAoirpAygWpYvb3twUAD-sm.jpg'
+            ),
+            array('name' => 'Obara', 'firstname' => 'Nicolas', 'category_id' => 2, 'email' => 'user@gmail.com',
+                'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S',
+                'avatar' => 'Qoo8OCzsNSO8ZPi5VyN6MAoirpAygWpYvb3twUAD.jpg',
+                'avatar_sm' => 'Qoo8OCzsNSO8ZPi5VyN6MAoirpAygWpYvb3twUAD-sm.jpg'
+            ),
+            array('name' => 'Doe', 'firstname' => 'John', 'category_id' => 1, 'email' => 'prof@gmail.com',
+                'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S',
+                'avatar' => 'Qoo8OCzsNSO8ZPi5VyN6MAoirpAygWpYvb3twUAD.jpg',
+                'avatar_sm' => 'Qoo8OCzsNSO8ZPi5VyN6MAoirpAygWpYvb3twUAD-sm.jpg'
+            ),
+            array('name' => 'Lafont', 'firstname' => 'Emma', 'category_id' => 2, 'email' => 'user1@gmail.com',
+                'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S',
+                'avatar' => 'JBu08aFGqY1ohbYO4Q2SRn5gHMcEcUjpFkXRO7Ig.jpg.jpg',
+                'avatar_sm' => 'JBu08aFGqY1ohbYO4Q2SRn5gHMcEcUjpFkXRO7Ig-sm.jpg'
+            ),
+            array('name' => 'Labat', 'firstname' => 'Laurent', 'category_id' => 2, 'email' => 'user2@gmail.com',
+                'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S',
+                'avatar' => 'obXfeCzUK7HStqyKcSPQ6SsX8naiErXulYXFx7f7.jpg',
+                'avatar_sm' => 'obXfeCzUK7HStqyKcSPQ6SsX8naiErXulYXFx7f7-sm.jpg'
+            ),
+            array('name' => 'Durand', 'firstname' => 'Tom', 'category_id' => 2, 'email' => 'user3@gmail.com',
+                'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'
+            ),
+            array('name' => 'Dupont', 'firstname' => 'Louis', 'category_id' => 2, 'email' => 'user4@gmail.com',
+                'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S',
+                'avatar' => 'TjO7yr8EuEl00pAMGhHQFdSsExsk015pava3TfIU.png',
+                'avatar_sm' => 'TjO7yr8EuEl00pAMGhHQFdSsExsk015pava3TfIU-sm.png'
+                )
         ));
     }
 }
