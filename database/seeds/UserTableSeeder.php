01a57629c4fbb6df12ec133a41e8f020c8274c6d
prudywsh <antoineprudhomme5@gmail.com>
2016-01-09 17:15:04 +0100

    Correction bug seeder user table

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,10 +15,10 @@ class UserTableSeeder extends Seeder
             array('name' => 'Prudhomme', 'firstname' => 'antoine', 'category_id' => 2, 'email' => 'antoineprudhomme5@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
             array('name' => 'Obara', 'firstname' => 'nicolas', 'category_id' => 2, 'email' => 'user@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
             array('name' => 'Doe', 'firstname' => 'John', 'category_id' => 1, 'email' => 'prof@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Lafont', 'Emma' => 'user1', 'category_id' => 2, 'email' => 'user1@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Labat', 'Laurent' => 'user2', 'category_id' => 2, 'email' => 'user2@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Durand', 'Tom' => 'user3', 'category_id' => 2, 'email' => 'user3@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
-            array('name' => 'Dupont', 'Louis' => 'user4', 'category_id' => 2, 'email' => 'user4@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S')
+            array('name' => 'Lafont', 'firstname' => 'user1', 'category_id' => 2, 'email' => 'user1@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
+            array('name' => 'Labat', 'firstname' => 'user2', 'category_id' => 2, 'email' => 'user2@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
+            array('name' => 'Durand', 'firstname' => 'user3', 'category_id' => 2, 'email' => 'user3@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S'),
+            array('name' => 'Dupont', 'firstname' => 'user4', 'category_id' => 2, 'email' => 'user4@gmail.com', 'password' => '$2a$10$n9qAMFM1fhRpdd9rcYdhEO8dkQQiqEm3D/Ro2KC9hgSMhZGejWw/S')
         ));
     }
 }
