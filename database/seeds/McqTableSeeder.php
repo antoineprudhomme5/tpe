b4af1690c7da979a8ee6710e58350c50888244c0
prudywsh <antoineprudhomme5@gmail.com>
2016-06-06 20:57:08 +0200

    Correction du seeder McqTableSeeder

--- database/seeds/McqTableSeeder.php
+++ database/seeds/McqTableSeeder.php
@@ -12,8 +12,8 @@ class McqTableSeeder extends Seeder
     public function run()
     {
         DB::table('mcq')->insert(array(
-            array("id" => 1, "name" => "quizz1", "playable" => true),
-            array("id" => 2, "name" => "quizz2", "playable" => true),
+            array("id" => 1, "name" => "quizz1", "playable" => true, "description" => "no description"),
+            array("id" => 2, "name" => "quizz2", "playable" => true, "description" => "no description"),
             array("id" => 3, "name" => "Synonyms", "playable" => true, "description" => "Find the synonym of the given word")
         ));
     }
